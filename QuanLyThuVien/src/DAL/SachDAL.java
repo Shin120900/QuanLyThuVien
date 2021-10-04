8c4dc5b04151ef4978bf1c0be74a87087658ae83
HieuNUCE <hieunuce2018@gmail.com>
2021-10-04 23:15:44 +0700

    hieu lai sua

--- QuanLyThuVien/src/DAL/SachDAL.java
+++ QuanLyThuVien/src/DAL/SachDAL.java
@@ -10,9 +10,9 @@ import MyException.MyException;
 public class SachDAL {
 	
 	private static SachDAL instance;
-	private ArrayList<SachDTO> dsSach;
+	private ArrayList<DauSachDTO> dsSach;
 	private SachDAL() {
-		dsSach=new ArrayList<SachDTO>();
+		dsSach=new ArrayList<DauSachDTO>();
 		loadResources();
 	}
 	
@@ -22,61 +22,29 @@ public class SachDAL {
 		return instance;
 	}
 	
-	public boolean isTrong(String mt) throws MyException{
-		for (SachDTO s: dsSach) {
-			if (s.getMaSach().equals(mt))
-				if (s.getTrangThai().equals("Trống"))
-					return true;
-				else 
-					return false;
-		}
-		
-		throw new MyException("Mã sách không hợp lệ");
-	}
-	
 	private void loadResources() {
 		try {
-			String query = new String("select * from sach");
+			String query = new String("select * from dbo.v_Sach");
 			ResultSet resultSet = DAL.getInstance().executeQueryToGetData(query);
 			
 			while(resultSet.next()) {
-				dsSach.add(new SachDTO(
-						resultSet.getObject(1).toString(), 
+				dsSach.add(new DauSachDTO(
+						Integer.parseInt( resultSet.getObject(1).toString()), 
 						resultSet.getObject(2).toString(), 
 						resultSet.getObject(3).toString(), 
 						resultSet.getObject(4).toString(),
 						resultSet.getObject(5).toString(),
-						Date.valueOf(resultSet.getObject(6).toString()),
-						resultSet.getObject(7).toString(),
-						resultSet.getObject(8).toString(),
-						Date.valueOf(resultSet.getObject(9).toString())));
+						Date.valueOf(resultSet.getObject(6).toString())));
 			}
 		}
 		catch(Exception ex){
 			ex.printStackTrace();
 		}
 	}
-	
-	private boolean isContain(SachDTO s) {
-		for (SachDTO item: dsSach)
-			if (item.getMaSach().equals(s.getMaSach()))
-				return true;
-		return false;
-	}
-	
-	public int addProcessing(SachDTO s) throws ContainException{
-		if (isContain(s))
-			throw new  ContainException("Sách đã tồn tại, hoặc mã sách đang bị trùng!");
-		String query = "insert into sach values(\"" 
-			+ s.getMaSach() +"\", \""
-			+ s.getTacGia() +"\", \""
-			+ s.getTenSach() +"\", \""
-			+ s.getTheLoai() +"\", \""
-			+ s.getNhaXuatBan() +"\", \""
-			+ s.getNgayNhap() +"\", \""
-			+ s.getGiaSach() +"\", \""
-			+ s.getTrangThai() +"\", \""
-			+ s.getNamXuatBan()+"\")";
+
+	public int addProcessing(DauSachDTO s) throws ContainException{
+		String query="INSERT INTO v_Sach(TENSACH,TENTG,TENTL,TENNXB,NAMXB) values("+s.getTenSach()+","+s.getTenTG()+","+
+				s.getTenTL()+","+s.getTenNXB()+","+s.getNamXB()+")";
 		int result = DAL.getInstance().executeQueryUpdate(query);
 		System.out.println(result);
 		if (result>0)
@@ -84,79 +52,68 @@ public class SachDAL {
 		return result;
 	}
 	
-	public void changeTrangThai(String maSach, String trangThai) throws MyException{
-		String query = "update sach set TrangThai = \""+trangThai+"\" where MaSach=\""+maSach+"\"";
-		int result = DAL.getInstance().executeQueryUpdate(query);
-		if (result == 0)
-			throw new MyException("Cập nhật trạng thái sách không thành công!");
-		for (SachDTO s: dsSach) {
-			if (maSach.equals(maSach)) {
-				s.setTrangThai(trangThai);
-				return;
-			}
-		}
-	}
 	
-	public SachDTO getSach(String maSach) {
-		for (SachDTO item:dsSach) {
-			if (item.getMaSach().equals(maSach))
+	public DauSachDTO getSach(int maDauSach) {
+		for (DauSachDTO item:dsSach) {
+			
+			if (item.getMaDauSach()==maDauSach)
 				return item;
 		}
 		return null;
 	}
 	
-	public String getTenSach(String maSach) {
-		for (SachDTO s: dsSach) {
-			if (maSach.equals(s.getMaSach()))
+	public String getTenSach(int maSach) {
+		for (DauSachDTO s: dsSach) {
+			if (maSach==s.getMaDauSach())
 				return s.getTenSach();
 		}
 		return "";
 	}
-	public ArrayList<SachDTO> getResources(){
+	public ArrayList<DauSachDTO> getResources(){
 		return  dsSach;
 	}
 
-	public int deleteProcessing(String s) {
-		int result = DAL.getInstance().executeQueryUpdate("delete from sach where MaSach=\""+ s +"\"");
-		if (result > 0)
-			for (int i = 0; i <dsSach.size(); i++) {
-				if (dsSach.get(i).getMaSach().equals(s))
-					dsSach.remove(i);
-			}
-		return result;
-	}
-
-	public String getThongTin(String maSach) {
-		for (SachDTO s: dsSach) {
-			if (s.getMaSach().equals(maSach))
-				return s.getMaSach()+s.getTenSach() + s.getTacGia();
-		}
-		return "";
-	}
-	
-	public int changeProcessing(SachDTO s) {
-		int result;
-		String query = "update sach set TenSach=\"" + s.getTenSach() 
-		+"\", GiaSach=\""  + s.getGiaSach() 
-		+"\", TacGia=\"" + s.getTacGia() 
-		+"\",  TheLoai=\"" + s.getTheLoai()
-		+"\", NhaXuatBan=\"" + s.getNhaXuatBan() 
-		+"\",  NgayNhap=\"" + s.getNgayNhap()
-		+ "\", TrangThai=\"" + s.getTrangThai() 
-		+ "\", NamXuatBan=\"" + s.getNamXuatBan() 
-		+ "\" where MaSach=\"" + s.getMaSach()+"\"";
-		//System.out.println("Query = "+ query);
-		result = DAL.getInstance().executeQueryUpdate(query);
-		
-		if (result > 0)
-			for (int i = 0; i<dsSach.size(); i++) {
-				SachDTO e = dsSach.get(i);
-				if (e.getMaSach().equals(s.getMaSach()))
-				{
-					dsSach.set(i, s);
-					break;
-				}
-			}
-		return result;
-	}
+//	public int deleteProcessing(String s) {
+//		int result = DAL.getInstance().executeQueryUpdate("delete from sach where MaSach=\""+ s +"\"");
+//		if (result > 0)
+//			for (int i = 0; i <dsSach.size(); i++) {
+//				if (dsSach.get(i).getMaSach().equals(s))
+//					dsSach.remove(i);
+//			}
+//		return result;
+//	}
+//
+//	public String getThongTin(String maSach) {
+//		for (SachDTO s: dsSach) {
+//			if (s.getMaSach().equals(maSach))
+//				return s.getMaSach()+s.getTenSach() + s.getTacGia();
+//		}
+//		return "";
+//	}
+//	
+//	public int changeProcessing(SachDTO s) {
+//		int result;
+//		String query = "update sach set TenSach=\"" + s.getTenSach() 
+//		+"\", GiaSach=\""  + s.getGiaSach() 
+//		+"\", TacGia=\"" + s.getTacGia() 
+//		+"\",  TheLoai=\"" + s.getTheLoai()
+//		+"\", NhaXuatBan=\"" + s.getNhaXuatBan() 
+//		+"\",  NgayNhap=\"" + s.getNgayNhap()
+//		+ "\", TrangThai=\"" + s.getTrangThai() 
+//		+ "\", NamXuatBan=\"" + s.getNamXuatBan() 
+//		+ "\" where MaSach=\"" + s.getMaSach()+"\"";
+//		//System.out.println("Query = "+ query);
+//		result = DAL.getInstance().executeQueryUpdate(query);
+//		
+//		if (result > 0)
+//			for (int i = 0; i<dsSach.size(); i++) {
+//				SachDTO e = dsSach.get(i);
+//				if (e.getMaSach().equals(s.getMaSach()))
+//				{
+//					dsSach.set(i, s);
+//					break;
+//				}
+//			}
+//		return result;
+//	}
 }
